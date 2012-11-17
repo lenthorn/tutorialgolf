3d38bc7a43b562cb66c7b78d286af99663c3f5f7
lenthorn <lenthorn592@gmail.com>
2012-11-17 10:45:07 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Golfshop::Application.config.secret_token = '87fe9c814219ee8db803aac86d9d909de8bd3f8bb6c73ef93ab870ab8caf8995eee3f576273f826389155ae977fc5a7f0f2e32d49d40207d313f57a587313bd0'
